c52195ee1e17b10d470b5ac4e3b92f4f751cd806
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-08 12:47:27 -0300

    ensure LoginRouter returns 500 if an invalid EmailValidator is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -201,4 +201,34 @@ describe('login Router', () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new InvalidParamError('email'));
     });
+
+    test('should return 500 if no EmailValidator is provided', async () => {
+        const authUseCaseSpy = makeAuthUseCase();
+        const sut = new LoginRouter(authUseCaseSpy);
+        const httpRequest = {
+            body: {
+                email: 'any_email@gmail.com',
+                password: 'any_password',
+            },
+        };
+
+        const httpResponse = await sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(500);
+        expect(httpResponse.body).toEqual(new ServerError());
+    });
+
+    test('should return 500 if EmailValidator has no IsValid method', async () => {
+        const authUseCaseSpy = makeAuthUseCase();
+        const sut = new LoginRouter(authUseCaseSpy, {});
+        const httpRequest = {
+            body: {
+                email: 'any_email@gmail.com',
+                password: 'any_password',
+            },
+        };
+
+        const httpResponse = await sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(500);
+        expect(httpResponse.body).toEqual(new ServerError());
+    });
 });
